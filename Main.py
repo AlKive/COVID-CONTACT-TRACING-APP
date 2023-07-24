624141bfe53f5f9fa1012cd7b52a26220e22af04
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-24 17:39:33 +0800

    cleared modules and imported tkinter

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 # import the necessary module
-from pyfiglet import figlet_format
-import pygame
-from termcolor import colored
-import pyfiglet
-from colorama import Back, Fore, Style, init
-import time
+import tkinter
+import tkinter as tk
+from tkinter import ttk
+
+mf = tkinter.Tk()
+
+mf.mainloop()
 
--- Tracer/Contact.py
+++
@@ -1,9 +0,0 @@
-# import the necessary module
-from pyfiglet import figlet_format
-import pygame
-from termcolor import colored
-import pyfiglet
-from colorama import Back, Fore, Style, init
-import time
-
-
