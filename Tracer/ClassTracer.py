caf6293b67ed23cbb0e2ca936b803993d6b8223d
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-27 02:27:24 +0800

    Last name label and entry widget

--- Tracer/ClassTracer.py
+++ Tracer/ClassTracer.py
@@ -26,15 +26,21 @@ class Tracer:
           self.self  = self
     
 #create functions for getting user's input using tkinter entry with label
+
+    #first name label and entry
     def firstName(self):
-        #first name label and entry
         self.FIRST_NAME = tkinter.Label(PERSONAL_INFORMATION, text="First Name")
         self.FIRST_NAME.grid(row=0, column=0)
         self.FIRST_NAME_INPUT = tkinter.Entry(PERSONAL_INFORMATION)
         self.FIRST_NAME_INPUT.grid(row=0, column=1)
     
     #last name label and entry
-    
+    def lastName(self):
+        self.LAST_NAME = tkinter.Label(PERSONAL_INFORMATION, text="Last Name")
+        self.LAST_NAME.grid(row=1, column=0)
+        self.LastName_INPUT = tkinter.Entry(PERSONAL_INFORMATION)
+        self.LastName_INPUT.grid(row=1, column=1)
+
     #Gender label and entry
     
     #last name label and entry
