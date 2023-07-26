0c2da548c713fcdef8e78bdaacbc73ee0cbba52a
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-27 02:19:51 +0800

    init and instatiate variable

--- Tracer/ClassTracer.py
+++ Tracer/ClassTracer.py
@@ -22,32 +22,34 @@ PERSONAL_INFORMATION.pack(side=TOP,padx=50, pady=30, fill= "both", expand= True)
 #create class
 class Tracer:
     #init function & create instance of variable
+    def __init__(self):
+          self.self  = self
     
-    #create functions for getting user's input using tkinter entry with label
+#create functions for getting user's input using tkinter entry with label
 
-        #first name label and entry
-        
-        #last name label and entry
-        
-        #Gender label and entry
-        
-        #last name label and entry
-       
-        #Age label and entry
-        
-        #Nationality label and entry
-
-        #Vaccination Status label and entry
-
-        #Phone Number label and entry
-        
-        #put terms and condition checkbox
- 
-        #fix spacing of widgets
-  
-        #create a save/add function for all user's info into a database ( i prefer excel as my database)
-
-        #create save/add button               
+    #first name label and entry
     
-        #create search button
-            
+    #last name label and entry
+    
+    #Gender label and entry
+    
+    #last name label and entry
+    
+    #Age label and entry
+    
+    #Nationality label and entry
+
+    #Vaccination Status label and entry
+
+    #Phone Number label and entry
+    
+    #put terms and condition checkbox
+
+    #fix spacing of widgets
+
+    #create a save/add function for all user's info into a database ( i prefer excel as my database)
+
+    #create save/add button               
+
+    #create search button
+        
