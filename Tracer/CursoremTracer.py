2bbbde09b30e92e47b2d94396f728b66ec173cd4
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-07-27 21:52:04 +0800

    imported tkinter and class, created an object of a class

--- Tracer/CursoremTracer.py
+++ Tracer/CursoremTracer.py
@@ -1,6 +1,11 @@
-#import tkinter
-# import class 
+#import tkinter and Tracer class
+from tkinter import*
+from covid_back_up import Tracer
+import tkinter
+ws=Tk()
+
 
 #create object
+al =Tracer()
 
 #run object functions        
